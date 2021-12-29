abf7bea6ff0d488cd212291d736d33a048f0e65c
lluis-fontbote <lluis.fontbote@insbaixcamp.cat>
2021-12-29 20:47:56 +0100

    user index

--- app/Http/Controllers/Back/UserController.php
+++ app/Http/Controllers/Back/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Back;
 
+use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -13,7 +15,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::paginate(10);
+        return view('back.user.index', compact('users'));
     }
 
     /**
---
+++ resources/views/back/user/index.blade.php
@@ -0,0 +1,68 @@
+@extends('back.mainLayout')
+
+@section('css')
+    <style>
+        .user-photo {
+            border-radius: 100%;
+            height: 70px; 
+            width: 80px;
+        }
+    </style>
+@endsection
+
+@section('content')
+<div class="container-fluid px-4">
+    <h1 class="mt-4">Usuarios</h1>
+    <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item"><a href="">Panel</a></li>
+        <li class="breadcrumb-item active">Usuarios</li>
+    </ol>
+    <div class="card mb-4">
+        <div class="card-body">
+            Aquí se muestran todos los usuarios registrados.
+        </div>
+    </div>
+    <div class="card mb-4">
+        <div class="card-header">
+            <i class="fas fa-table me-1"></i>
+            Listado de usuarios de la plataforma
+        </div>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Foto</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Correo</th>
+                        <th>Es admin</th>
+                        <th>Fecha de alta</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody class="table-hover">
+                    @foreach ($users as $user)
+                    <tr>
+                        <td><img src="{{ asset('storage/userPhotos') . '/' . $user->photo }}" class="user-photo" alt="Foto del usuario."></td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->surname }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->is_admin ? 'Sí' : 'No' }}</td>
+                        <td>{{ $user->created_at }}</td>
+                        <td>
+                            <a href='{{ route('back.user.edit', $user->id) }}' class='btn btn-primary m-r-lem'>
+                                <i class="fas fa-edit"></i> Editar
+                            </a> 
+                            <a href='{{ route('back.product.destroy', $user->id) }}' class='btn btn-danger'>
+                                <i class="fas fa-trash-alt"></i> Eliminar
+                            </a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            {{ $users->links() }}
+        </div>
+    </div>
+</div>
+@endsection
