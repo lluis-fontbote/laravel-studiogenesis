247e09ec5be12f3d3eeb6d4041c88bb976d68edf
lluis-fontbote <lluis.fontbote@insbaixcamp.cat>
2022-01-01 17:16:26 +0100

    fix: forgot importing back template scripts

--- resources/views/back/mainLayout.blade.php
+++ resources/views/back/mainLayout.blade.php
@@ -139,6 +139,7 @@
     <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
     <script src="js/datatables-simple-demo.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/back-template.js') }}"></script>
     @yield('js')
 </body>
 </html>
